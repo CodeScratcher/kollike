af093ab10012e215eae35f0c86de2800ff98b538
Arielle F <bb8andr2d2forlife@gmail.com>
2023-01-21 19:35:38 -0500

    Added basic hello world

---
+++ src/test/java/com/fisher/kollike/KollikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisher.kollike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KollikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
